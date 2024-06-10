cd868b7029bb073a27036f5f50c1ed447240ccff
Brasero <brandonricci4@gmail.com>
2024-06-10 11:32:24 +0200

    data

--- server.js
+++ server.js
@@ -13,35 +13,6 @@ server.use(express.static(staticPath))
 //   res.sendFile(path.join(staticPath, 'exemple.jpg'))
 // })
 
-server.get('/', (req,res) => {
-  res.send(`
-    <h1>Welcome</h1>
-    <div><a href="/user/Paul">/user/Paul</a></div>
-    <div><a href="/query?search=banana&limit=5">/query?search=banana&limit=5</a></div>
-    
-    <img src="/img/exemple.jpg" alt="img"/>
-  `)
-})
-
-
-server.get("/user/:name", (req, res) => {
-  const {name} = req.params
-  
-  res.send(`
-    <div>User is ${name}</div>
-    <a href="/" >Home</a>
-  `)
-})
-
-server.get('/query', (req, res) => {
-  const {search, limit} = req.query
-  res.send(`
-    <h1>Search</h1>
-    <div>You're looking for ${search}, ${limit} times</div>
-    <a href="/">Home</a>
-  `)
-})
-
 server.get("*", (req, res) => {
   res.status(404).send(`
     <h1>Not found</h1>
