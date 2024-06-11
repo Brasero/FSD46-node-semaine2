6a30df6070d83e5396be0b597a24345663e8389f
Brasero <brandonricci4@gmail.com>
2024-06-11 12:12:13 +0200

    presentation des sessions express

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from 'node:path';
+import session from 'express-session';
 import {counterMiddleware, loggerMiddleware} from "./middleware.js";
 
 const server = express();
@@ -8,12 +9,34 @@ const port = 8000;
 const __dirname = import.meta.dirname
 const staticPath = path.join(__dirname, 'public')
 
+server.use(session({
+  name: "test",
+  secret: "simple",
+  resave: false,
+  saveUninitialized: true
+}))
 server.use(express.static(staticPath))
 server.use(express.urlencoded({extended: false}))
-server.use(counterMiddleware)
-server.use(loggerMiddleware)
 
 server.all("*", (req, res) => {
+  if (req.session.counter) {
+    req.session.counter++
+  } else {
+    req.session.counter = 1
+  }
+  
+  console.log(req.session.counter)
+  
+  // Reset de la session et des infos qu'elle contient
+  req.session.regenerate((err) => {
+    console.log(err)
+  })
+  
+  // Détruit la session et toutes les infos qu'elle contient
+  req.session.destroy((err) => {
+    console.log(err)
+  })
+  
   res.send(`
     <code>
       <a href="/">/</a><br>
