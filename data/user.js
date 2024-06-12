78cdaaa69f624a4577c2c5a1ad9d9f4d3bdf5d1e
Brasero <brandonricci4@gmail.com>
2024-06-12 17:43:18 +0200

    preparation

--- data/user.js
+++
@@ -1,6 +0,0 @@
-const user = {
-  login : "Alan",
-  password : "73a056240baf641c8dc2c9bab20e0c2b457bd6e4" // correspond à "4l4n"
-}
-
-export default user
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,5 @@
 import express from 'express'
-import {getForm, getSecurePage, login, logout} from "../controller/user.controller.js";
-import authMiddleware from '../middleware/auth.js';
 
 const router = express.Router()
-
-router.get('/', getForm)
-router.post('/login', login)
-router.get('/secure', authMiddleware, getSecurePage)
-router.get('/logout', authMiddleware, logout)
 
 export default router
