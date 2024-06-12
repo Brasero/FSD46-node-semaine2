af4795565144dd2fa05f63cae15a1fc551919ead
Brasero <brandonricci4@gmail.com>
2024-06-12 12:01:48 +0200

    correction login Challenge

---
+++ controller/user.controller.js
@@ -0,0 +1,47 @@
+import fs from 'node:fs'
+import path from 'node:path'
+import user from '../data/user.js';
+import crypto from "crypto-js";
+
+const cwd = process.cwd()
+const viewPath = path.join(cwd, 'views')
+
+export function getForm(req,res) {
+  const html = fs.readFileSync(path.join(viewPath, 'form.html'), {encoding: "utf8"})
+  res.send(html)
+}
+
+export function login(req,res) {
+  const {name, password} = req.body
+  
+  if (!name || name.trim() === "" || !password || password.trim() === "") {
+    req.session.auth = false
+    console.log('connexion failed')
+    res.status(403).send("Malformed request")
+    return
+  }
+  if (name !== user.login || crypto.SHA1(password).toString() !== user.password) {
+    req.session.auth = false
+    console.log('connexion failed')
+    res.status(401).send("Wrong credentials")
+    return
+  }
+  console.log('connexion succeeded')
+  req.session.auth = true
+  res.redirect('/secure')
+}
+
+export function getSecurePage(req, res) {
+  const html = fs.readFileSync(path.join(viewPath, 'secure.html'), {encoding: 'utf8'})
+  res.send(html)
+}
+
+export function logout (req,res) {
+  req.session.destroy((err) => {
+    if (err) {
+      res.redirect('/secure')
+      return
+    }
+    res.redirect("/")
+  })
+}
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import path from 'node:path';
 import session from 'express-session';
-import {counterMiddleware, loggerMiddleware} from "./middleware.js";
+import routes from "./routes/index.js";
 
 const server = express();
 const port = 8000;
@@ -17,34 +17,8 @@ server.use(session({
 }))
 server.use(express.static(staticPath))
 server.use(express.urlencoded({extended: false}))
+server.use(routes)
 
-server.get("/", (req, res) => {
-  if (req.session.counter) {
-    req.session.counter++
-  } else {
-    req.session.counter = 1;
-  }
-  
-  if (req.session.counter >= 10){
-    res.redirect("/check")
-    return
-  }
-  
-  res.send({page: "/", counter: req.session.counter})
-})
-
-server.get('/check', (req, res) => {
-  res.send(`
-    page: /check
-    counter: ${req.session.counter}
-    <a href="/delete">Retry</a>
-  `)
-})
-
-server.get('/delete', (req, res) => {
-  req.session.regenerate(() => {})
-  res.redirect('/')
-})
 
 server.listen(port, () => {
   console.log(`Listening on port ${port}`)
