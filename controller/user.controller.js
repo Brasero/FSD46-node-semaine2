83a3f679e5e0f16b81b51879c011175690ba27d0
Brasero <brandonricci4@gmail.com>
2024-06-12 16:45:17 +0200

    JWT

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,6 +2,10 @@ import fs from 'node:fs'
 import path from 'node:path'
 import user from '../data/user.js';
 import crypto from "crypto-js";
+import jwt from 'jsonwebtoken';
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const cwd = process.cwd()
 const viewPath = path.join(cwd, 'views')
@@ -15,19 +19,21 @@ export function login(req,res) {
   const {name, password} = req.body
   
   if (!name || name.trim() === "" || !password || password.trim() === "") {
-    req.session.auth = false
+    req.session.token = false
     console.log('connexion failed')
     res.status(403).send("Malformed request")
     return
   }
   if (name !== user.login || crypto.SHA1(password).toString() !== user.password) {
-    req.session.auth = false
+    req.session.token = false
     console.log('connexion failed')
     res.status(401).send("Wrong credentials")
     return
   }
+  const token = jwt.sign({userId: 1}, process.env.JWT_SECRET, {algorithm: "HS256"});
+  
   console.log('connexion succeeded')
-  req.session.auth = true
+  req.session.token = token
   res.redirect('/secure')
 }
 
--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,15 @@
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+dotenv.config()
+
 export default function (req,res, next) {
-  if (req.session.auth) {
+  
+  if (req.session.token) {
+    const decoded = jwt.decode(req.session.token, process.env.JWT_SECRET)
+    if (!decoded) {
+      res.redirect('/')
+      return
+    }
     next()
     return
   }
