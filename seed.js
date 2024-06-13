e83c0ccf75a2d10f07d5afd512c4bd1603824c5d
Brasero <brandonricci4@gmail.com>
2024-06-13 22:51:30 +0200

    préparation énoncé

--- seed.js
+++
@@ -1,9 +0,0 @@
-import Product from "./model/product.js";
-import products from './data/products.js';
-
-(async function seed() {
-  await Product.deleteMany({});
-  await Product.insertMany(products)
-  console.log("Seed inserted")
-  process.exit(0);
-})()
